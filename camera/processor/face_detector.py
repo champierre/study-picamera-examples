5902ade4b4bbb53e25aae9996b4baf8543c39f2d
Junya Ishihara <junya@champierre.com>
2018-08-07 20:21:57 +0900

    Fix error "bad argument type for built-in operation".

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -43,7 +43,7 @@ class FaceDetector(object):
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(frame,len(faces),(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+        cv2.putText(frame, str(len(faces)), (10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
 
         # frameを戻り値として返す
         return frame
